b4e6ffec62f2be607ca8e2cc72869490dbc66012
dizza@data.ai <dizza@data.ai>
2024-06-26 00:31:56 +0100

    refactor code and clean files

--- milestone_5.py
+++ milestone_5.py
@@ -1,19 +1,3 @@
-# Create a function that will run all the code to run the game as expected. You should begin by creating a new script called milestone_5.py. Copy all the codes in milestone_4.py file into the newly created milestone_5.py file.
-
-
-# Step 1:
-# Create a function called play_game that takes word_list as a parameter. Inside the function, write the code for the following steps
-
-# Create a variable called num_lives and assign it to 5
-# Create an instance of the Hangman class. Do this by calling the Hangman class and assign it to a variable called game
-# Pass word_list and num_lives as arguments to the game object
-# Create a while loop and set the condition to True. In the body of the loop, do the following:
-# Check if the num_lives is 0. If it is, that means the game has ended and the user lost. Print a message saying 'You lost!'
-# Next, check if the num_letters is greater than 0. In this case, you would want to continue the game, so you need to call the ask_for_input method.
-# If the num_lives is not 0 and the num_letters is not greater than 0, that means the user has won the game. Print a message saying 'Congratulations. You won the game!'
-# Step 2:
-# Outside the function, call your play_game function to play your game. Don't forget to pass in your list of words as argument to the function.
-
 import random
 import re
 
@@ -27,7 +11,6 @@ class Hangman:
         self.num_letters = len(set( self.letters))
         self.num_lives = num_lives
         self.word_list = word_list
-        # self.word_guessed = re.sub(r'[a-zA-Z]', '_', self.word)
         self.list_of_guesses = []
 
     
@@ -46,9 +29,6 @@ class Hangman:
                 print("Sorry, {letter} is not in the word.")
                 print(f"You have {self.num_lives} lives left.")
 
-
-
-    
     def ask_for_input(self):
         
         guess = input("Enter a letter:")
